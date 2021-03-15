7e645081e5cf1323ae5a4aef07a57d7fd4bcdcc6
Tony <tony@cooldark.com>
2021-03-15 16:05:03 +0000

    Add TCP server and better CLI controls

--- README.md
+++ README.md
@@ -1,5 +1,77 @@
-#AIS dispatcher
+# AIS dispatcher
 
-Reads serial port and sends contents to UDP destinations specified in the script.
+Takes AIS data from a serial or UDP input.
 
-Note that this can include localhost, which allows other tools run on the same machie (for example, the aishub dispatcher) to read this as input.
+Starts threaded TCP server that accepts connections to forward AIS data on to.
+
+Dispatches AIS data in UDP payloads to list of destinations.*
+
+*Note that this can include localhost, which allows other tools run on the same machine (for example, the aishub dispatcher) to read this as input.
+
+## Prerequisits
+
+python3
+
+...and the following pip3 modules:
+
+`sudo pip3 install pyserial click`
+
+## Usage
+
+Example:
+`./dispatcher.py --host 192.168.1.4 1371 --serial-port /dev/serial0`
+
+Here the IP is the address of the interface on the local machine from which to serve (in this example, using port 1371).
+
+The complete list of arguments are given by the --help.
+
+```
+Usage: dispatcher.py [OPTIONS]
+
+Options:
+  --host <TEXT INTEGER>...      Host <ip> <port>  [required]
+  --serial-port TEXT            Serial port device
+  --serial-rate INTEGER         Serial port baudrate
+  --udp-src <TEXT INTEGER>...   UDP source <ip> <port>
+  --udp-dest <TEXT INTEGER>...  UDP destination <ip> <port>
+  --help                        Show this message and exit.
+```
+
+## As a systemd service
+
+Typically a systemd service would look like this:
+
+```
+[Unit]
+Description=AIS Dispatcher
+After=network-online.target
+Wants=network-online.target systemd-networkd-wait-online.service
+
+StartLimitIntervalSec=500
+StartLimitBurst=5
+
+[Service]
+Restart=on-failure
+RestartSec=5s
+
+StandardOutput=journal+console
+ExecStart=/usr/bin/python3 /home/pi/AIS/dispatcher.py --host 192.168.1.4 1371 --serial-port /dev/serial0
+
+[Install]
+WantedBy=multi-user.target
+```
+
+For example - put this in file */usr/lib/systemd/system/ais-dispatcher.service* and enable with:
+
+```
+sudo systemctl enable /usr/lib/systemd/system/ais-dispatcher.service
+sudo systemctl start ais-dispatcher.service
+```
+
+Status of the started unit can then be checked with either:
+```
+systemctl status ais-dispatcher.service
+journalctl -u ais-dispatcher.service
+```
+
+At this point you should be able to get an AIS stream just by telnetting to port 1371 on the configured interface.
--- dispatcher.py
+++ dispatcher.py
@@ -1,36 +1,159 @@
 #!/usr/bin/python3
 from socket import socket, AF_INET, SOCK_DGRAM
 from serial import Serial
+from threading import Thread, Event
+from queue import Queue
+from socketserver import BaseRequestHandler, ThreadingMixIn, TCPServer
+from time import sleep
+import click # for CLI args
 
-# if serial module is missing then: sudo pip3 install pyserial
+# this class handles inbound TCP connections and is threaded
+# IE one per opened socket
+class ThreadedAISTCPHandler(BaseRequestHandler):
+    def setup(self):
+        print("New client, creating queue.")
+        # subscribe to input messages
+        self.q = Queue()
+        self.server.subscribers.append(self.q)
 
-# this machine's interface to bind to
-SERVER=('192.168.1.4', 5001)
+    def handle(self):
+        try:
+            while True:
+                data = self.q.get()
+#                print("Sending", data)
+                self.request.sendall(data)
+                self.q.task_done()
+        except BrokenPipeError as e:
+            print("Connection error:", e)
 
-# input serial port device
-INPUT = '/dev/serial0'
+    def finish(self):
+        print("Lost client, destroying queue.")
+        self.server.subscribers.remove(self.q)
 
-# UDP destinations
-DESTINATIONS = [
-	['192.168.1.3', 5001],
-	['192.168.1.11', 5001],
-	['localhost',   5001]
-]
+# this overloads the ThreadedTCPServer so that an additional argument can be passed in,
+# in our case the subscriber queue list
+class ThreadedTCPServer(ThreadingMixIn, TCPServer):
+    def __init__(self, host_port_tuple, streamhandler, subscribers):
+        super().__init__(host_port_tuple, streamhandler)
+        self.subscribers = subscribers
 
-ser = Serial(INPUT, 38400)
+# listen to a serial port and send any data to all subscribers
+def serialListener(subscribers, stop_event, serial_port='/dev/serial0', serial_rate=38400):
+    # give the main process time to set stop_event if port isn't available
+    sleep(0.5)
 
-# create a UDP socket
-s = socket(AF_INET, SOCK_DGRAM)
+    print("Listening to serial device", serial_port)
+    ser = Serial(serial_port, serial_rate)
 
-# bind the socket to the port
-s.bind(SERVER)
+    while not stop_event.is_set():
+        data = ser.readline()
+        print("SER:", data.decode('utf-8').rstrip())
+        [q.put(data) for q in subscribers]
 
-print("Reading serial... (ctrl-c to exit)\n")
-while True:
-    data = ser.readline()
-    print("SER:", data)
+# listen to a UDP port and send any data to all subscribers
+def udpListener(subscribers, stop_event, host):
+    # give the main process time to set stop_event if port isn't available
+    sleep(0.5)
 
-    for host in DESTINATIONS:
-#        print('Dispatching to', host)
-        s.sendto(data, tuple(host))
+    print("Listening to UDP on", host)
+    # Create a UDP input socket
+    s = socket(AF_INET, SOCK_DGRAM)
+    # Bind the socket to the port
+    s.bind(host)
 
+    while not stop_event.is_set():
+        data, address = s.recvfrom(4096)
+        print("UDP:", data.decode('utf-8').rstrip())
+        [q.put(data) for q in subscribers]
+
+# subscribe to input AIS streams and send out to UDP destination(s)
+def udpDispatcher(server, destinations, subscribers, stop_event):
+    # give the main process time to set stop_event if port isn't available
+    sleep(0.5)
+
+    # subscribe
+    q = Queue()
+    subscribers.append(q)
+
+    # create a UDP socket to send with
+    s = socket(AF_INET, SOCK_DGRAM)
+
+    # bind the socket to the UDP port
+    s.bind(server)
+
+    while not stop_event.is_set():
+        data = q.get()
+        [s.sendto(data, host) for host in destinations]
+        q.task_done()
+
+    # unsubscribe
+    subscribers.remove(self.q)
+
+@click.command()
+@click.option('--host', required=True, type=(str, int), help='Host <ip> <port>')
+@click.option('--serial-port', type=str, default='', help='Serial port device')
+@click.option('--serial-rate', type=int, default=38400, help='Serial port baudrate')
+@click.option('--udp-src', type=(str, int), default=('',0), help='UDP source <ip> <port>')
+@click.option('--udp-dest', type=(str, int), default=('',0), help='UDP destination <ip> <port>')
+def dispatcher(
+    host,
+    serial_port,
+    serial_rate,
+    udp_src,
+    udp_dest
+    ):
+
+    # UDP destinations
+    if udp_dest[0]:
+        udp_destinations = [ udp_dest ]
+    else:
+        # todo, read from config file instead of hardcode
+        udp_destinations = [
+            ('192.168.1.3', 1371),
+            ('192.168.1.11', 1371),
+            ('localhost', 1371)
+        ]
+
+    # these queues exist between listeners (either serial or UDP input threads) and dispatchers (UDP outputs, or TCP outputs)
+    subscribers = []
+
+    # used to signal to listener thread to stop running
+    stop_event=Event()
+    
+    print("Starting listeners...")
+    if udp_src[0]:
+        listener = Thread(target=udpListener, args=(subscribers, stop_event, udp_src))
+        listener.daemon = True
+        listener.start()
+
+    if serial_port:
+        listener = Thread(target=serialListener, args=(subscribers, stop_event, serial_port, serial_rate))
+        listener.daemon = True
+        listener.start()
+
+    print("Starting UDP dispatcher...")
+    dispatcher = Thread(target=udpDispatcher, args=(host, udp_destinations, subscribers, stop_event)) 
+    dispatcher.start()
+
+    print("Starting TCP server on", host)
+    try:
+        server = ThreadedTCPServer(host, ThreadedAISTCPHandler, subscribers)
+        with server:
+            ip, port = server.server_address
+
+            # start a thread with the server -- that thread will then start one
+            # more thread for each request
+            server_thread = Thread(target=server.serve_forever)
+            # exit the server thread when the main thread terminates
+            server_thread.daemon = True
+            server_thread.start()
+            while True:
+                sleep(10)
+
+    except (OSError, KeyboardInterrupt) as e:
+        print(e)
+        # stop threads!
+        stop_event.set()
+
+if __name__ == "__main__":
+    dispatcher()
